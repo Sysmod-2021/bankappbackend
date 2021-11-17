bc09e43bbaadf61893b19d4fefe5a7bceb327e00
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-11-17 21:39:32 +0200

    #24: Adding a test example with automatically generated object diagram

---
+++ src/test/java/BankTests.java
@@ -0,0 +1,22 @@
+import org.fulib.FulibTools;
+import org.junit.Test;
+
+public class BankTests {
+    @Test
+    public void testGameSetup() {
+        Bank bank = new Bank();
+        // TODO: add some customers
+        // TODO: add an admin
+        // TODO: add some transactions
+
+        FulibTools.objectDiagrams().dumpSVG("docs/objects/bank_objects.svg", bank);
+    }
+}
+
+
+
+
+
+
+
+
