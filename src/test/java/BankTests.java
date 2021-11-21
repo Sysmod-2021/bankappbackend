3f31f0bcd70bfa95dc8317d020ec40506febc2f3
Worraanong Chanchaijak <c.worraanong@gmail.com>
2021-11-21 20:26:45 +0200

    Add an Object diagram for simple transfer scenario

--- src/test/java/BankTests.java
+++ src/test/java/BankTests.java
@@ -1,15 +1,25 @@
-//import org.fulib.FulibTools;
+import org.fulib.FulibTools;
 import org.junit.Test;
 
 public class BankTests {
     @Test
-    public void testGameSetup() {
-        Bank bank = new Bank();
-        // TODO: add some customers
-        // TODO: add an admin
-        // TODO: add some transactions
+    public void shouldGenerateObjectDiagrams() {
+    	java.util.Currency eur = java.util.Currency.getInstance("EUR");
+		
+    	Bank bank = new Bank();
+ 
+    	Administrator admin = new Administrator(bank, "Admin", "ALice", "admin@bank.ee", "secure_p@ssw0|2d");
 
-//        FulibTools.objectDiagrams().dumpSVG("docs/objects/bank_objects.svg", bank);
+    	Customer customer = new Customer(bank, "John", "Doe", "john@doe.ee", "pass1234", "400000000001");
+        Account customerBankAccount  = new Account(bank, customer, eur, 100f);
+		
+		Customer beneficiary = new Customer(bank, "Bob", "Jackson", "bobby@tt.ee", "secret", "400000000002");
+		Account destinationBankAccount  = new Account(bank, beneficiary, eur, 0f);
+
+		Transaction transfer = new Transaction(bank, customerBankAccount, destinationBankAccount, Currency.EUR,  25.50f, "ref: 001");
+		transfer.execute();
+
+        FulibTools.objectDiagrams().dumpSVG("docs/objects/tranfermoney_objects.svg", transfer);
     }
 }
 
