8bf35b13f020f58716e6e819b8a26e561591ede5
Ihar Suvorau <ihar.suvorau@gmail.com>
2021-11-22 19:17:43 +0200

    Updating AccountTests

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,9 +1,5 @@
 public class Customer extends User {
-//    Might be redundant
-    private final String personalCode;
-
     public Customer(Bank bank, String firstName, String lastName, String email, String password, String personalCode) {
         super(bank, firstName, lastName, email, password);
-        this.personalCode = personalCode;
     }
 }
